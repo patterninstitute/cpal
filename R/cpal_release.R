c7e33f620a17dc2d955e6a8c11566a9e90d941d0
nunoaguileira <nunoaguileira1@gmail.com>
2025-08-20 11:23:12 +0100

    Add cpal_release() function and change how description displays version

---
+++ R/cpal_release.R
@@ -0,0 +1,14 @@
+#' @title CPAL Release Date
+#'
+#' @description This function retrieves the release date of the bundled
+#' CPAL terminology from the package version.
+#'
+#' @returns A [Date][base::Dates] object.
+#'
+#' @examples
+#' cpal_release()
+#'
+#' @export
+cpal_release <- function() {
+  as.Date(as.character(utils::packageVersion(utils::packageName())), format = "%Y-%m-%d")
+}
